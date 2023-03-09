0f85f0e331899d56b00e610e85f64c38af80948a
מוני ברוך <monybaruch@gmail.com>
2023-03-09 23:15:54 +0200

    fixing the button

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let randomNum1 = Math.floor(Math.random() * 6 + 1);
-let randomNumb2 = Math.floor(Math.random() * 6 + 1);
 function rollDice() {
+  let randomNum1 = Math.floor(Math.random() * 6 + 1);
+  let randomNumb2 = Math.floor(Math.random() * 6 + 1);
+
   document
     .querySelectorAll("img")[0]
     .setAttribute("src", `./images/dice${randomNum1}.png`);
@@ -8,11 +9,11 @@ function rollDice() {
     .querySelectorAll("img")[1]
     .setAttribute("src", `./images/dice${randomNumb2}.png`);
 
-  if (randomNum1 === randomNumb2) {
-    document.querySelector("h1").textContent = "Its a draw";
-  } else if (randomNum1 > randomNumb2) {
-    document.querySelector("h1").textContent = "Player 1 wins";
+  if (randomNum1 > randomNumb2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins";
   } else if (randomNum1 < randomNumb2) {
-    document.querySelector("h1").textContent = "Player 2 wins";
+    document.querySelector("h1").innerHTML = "Player 2 wins";
+  } else {
+    document.querySelector("h1").innerHTML = "Its a draw";
   }
 }
